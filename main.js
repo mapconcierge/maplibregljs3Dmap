e5fc2c421aa68c6ada3cddbdc50f3c033113cb85
Taichi FURUHASHI <mapconcierge@gmail.com>
2025-11-14 16:58:58 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const map = new maplibregl.Map({
   container: "map",
   center: [139.107275, 35.982309], // 中心座標
-  zoom: 10, // ズームレベル
+  zoom: 15, // ズームレベル
   style: {
     // スタイル仕様のバージョン番号。8を指定する
     version: 8,
